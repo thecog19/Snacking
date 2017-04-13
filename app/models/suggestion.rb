51f19c1b0492a1d5370ba0c0ee97a53175e8bb4a
Francisco Suero <suerofrancisco@yahoo.com>
2017-04-13 17:20:32 -0400

    suggestion model updated

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -2,4 +2,18 @@ class Suggestion < ApplicationRecord
   has_many :votes, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+
+  def self.suggestion_data(snackdata = SnackData)
+    api_suggestions = snackdata.optional_snacks
+    names = generate_names(api_suggestions) 
+    self.where('name IN (?)', name_list))
+  end
+
+  def self.generate_names(arr)
+    name_list = []
+    arr.each do |item|
+      name_list << item["name"]
+    end
+    name_list
+  end
 end
