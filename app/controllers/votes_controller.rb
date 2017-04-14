0fe418fb571d27bf86d33a3e639a94a50d1560f0
Francisco Suero <suerofrancisco@yahoo.com>
2017-04-14 14:47:32 -0400

    cookie initial setup

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,13 +3,24 @@ class VotesController < ApplicationController
   #write rake task to be run every month (via cron, to reset "selected") (will delete the suggestions table and the votes table)
 
   def index
+    if cookies[:votes].blank?
+      cookies[:votes] = {value: 3, :expires => end_of_month}
+
+    end
+    @votes = cookies[:votes]
     @permanent = SnackData.permanent_snacks
-    @suggestions = Suggestion.all
-    @suggestions_info = nil #this will contain all the data for each suggestion
+    @suggestions = Suggestion.suggestion_data
     @online = SnackAPI.new.online?
   end
 
   def create
+    respond_to :json
+    unless cookies[:votes] == 0 
+      cookies[:votes] = cookies[:votes].to_i - 1
+    else
+      "No votes left this month"
+    end
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   #our root is the vote index. As good a landing page as any!
   root 'votes#index'
+  resources :votes, only: [:create, :index]
 end
