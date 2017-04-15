292a53cd1125dd88cd64722d63832d990d2ea193
Francisco Suero <suerofrancisco@yahoo.com>
2017-04-15 12:20:00 -0400

    buiding controler

---
+++ app/controllers/suggestions_controller.rb
@@ -0,0 +1,8 @@
+class SuggestionsController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   #our root is the vote index. As good a landing page as any!
   root 'votes#index'
   resources :votes, only: [:create, :index]
+  resources :suggestions, only: [:create, :index]
 end
